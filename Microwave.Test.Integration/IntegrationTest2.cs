1d9ef9ee0dd64b9dbcd3ff89e3788d7dbe24ccab
nicho1991 <ladefoged1991@gmail.com>
2018-03-20 16:30:58 +0100

    test2 setup

--- Microwave.Test.Integration/IntegrationTest2.cs
+++ Microwave.Test.Integration/IntegrationTest2.cs
@@ -1,9 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using NUnit.Framework;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
     public class IntegrationTest2
     {
+        private IButton PowerButton;
+        private IButton TimeButton;
+        private IButton startCancel;
+
+        private IDoor Door;
+
+        private IDisplay Display;
+        private ICookController CookControl;
+        private ILight Light;
+
+        private IUserInterface UserInterface;
+
+        [SetUp]
+        public void setup()
+        {
+            PowerButton = new Button();
+            TimeButton = new Button();
+            startCancel = new Button();
+
+            Door = new Door();
+
+            Display = Substitute.For<IDisplay>();
+            CookControl = Substitute.For<ICookController>();
+            Light = Substitute.For<ILight>();
+
+            UserInterface = new UserInterface(PowerButton, TimeButton, startCancel, Door, Display, Light, CookControl);
+        }
     }
 }
