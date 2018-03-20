eb5ecc72bf870bbfae5084ac35e69eb11395a16e
nicho1991 <ladefoged1991@gmail.com>
2018-03-20 10:51:03 +0100

    test er sat op

---
+++ Microwave.Test.Integration/IntegrationTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MicrowaveOvenClasses.Boundary;
+
+using NUnit.Framework;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IntegrationTest1
+    {
+        //billede 2
+        [Test]
+        public void UserInterfaceButton()
+        {
+            IButton PowerButton = new Button();
+            var UserInterface = Substitute.For<IUserInterface>();
+            PowerButton.Pressed += new EventHandler(UserInterface.OnPowerPressed);
+
+            PowerButton.Press();
+            UserInterface.Received()
+
+        }
+    }
+}
---
+++ Microwave.Test.Integration/IntegrationTest2.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IntegrationTest2
+    {
+    }
+}
