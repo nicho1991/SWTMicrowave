f3e3b27cb08e6424c79abe6e28dfc687f13b08fe
Max <maxbj@live.dk>
2018-04-07 16:09:18 +0200

    .
    .

--- MicrowaveOvenClasses/Boundary/Timer.cs
+++ MicrowaveOvenClasses/Boundary/Timer.cs
@@ -45,7 +45,7 @@ namespace MicrowaveOvenClasses.Boundary
         {
             // One tick has passed
             // Do what I should
-            TimeRemaining -= 1;
+            TimeRemaining -= 1000;
             TimerTick?.Invoke(this, EventArgs.Empty);
 
             if (TimeRemaining <= 0)
